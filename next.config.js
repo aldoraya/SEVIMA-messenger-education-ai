798e11a83ef5a13f8bae62a855b20da39d217a3f
aldoraya <aldorayaalhakim@gmail.com>
2023-06-24 09:10:14 +0700

    Upload Folder & File

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    experimentals: {
+        appDir: true,
+    }
+}
 
 module.exports = nextConfig
